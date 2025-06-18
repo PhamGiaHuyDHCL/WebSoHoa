8f0e0ed33f48ec5821c9c781d736c5f836b6be2e
NhutNgoan <nhutngoanmt2004@gmail.com>
2025-06-18 22:30:00 +0700

    nntext.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Optionally add Chart.js 222222222222222222222222222-->
+    <!-- Optionally add Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
