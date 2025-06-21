8a861c1d65720b45de5e33ea59e753045fcbea48
nguyenhuonggiang <nhgiang.0412@gmail.com>
2025-06-21 11:04:49 +0700

    Update edit_taikhoan.php

--- views/taikhoan/edit_taikhoan.php
+++ views/taikhoan/edit_taikhoan.php
@@ -1,39 +1,39 @@
-<!-- Modal chỉnh sửa tài khoản -->
+<!-- chỉnh sửa tài khoản -->
 <div class="modal fade" id="editAccountModal" tabindex="-1" aria-labelledby="editAccountModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-      <form method="POST" action="edit_taikhoan_xuly.php" id="editAccountForm">
+      <form method="POST" id="editAccountForm">
         <div class="modal-header">
           <h5 class="modal-title" id="editAccountModalLabel">Chỉnh sửa tài khoản</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Đóng"></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
         <div class="modal-body">
           <input type="hidden" name="edit_id" id="editUserId">
           <div class="row mb-3">
             <div class="col-md-4">
-              <label for="editHoTen">Họ tên *</label>
-              <input type="text" name="hoten" id="editHoTen" class="form-control" required>
+              <label>Họ tên *</label>
+              <input name="hoten" id="editHoTen" class="form-control" required>
             </div>
             <div class="col-md-4">
-              <label for="editSdt">Số điện thoại *</label>
-              <input type="text" name="sdt" id="editSdt" class="form-control" required pattern="\d{10}">
+              <label>Số điện thoại *</label>
+              <input name="sdt" id="editSdt" class="form-control" required pattern="\d{10}">
             </div>
             <div class="col-md-4">
-              <label for="editCccd">Số CCCD *</label>
-              <input type="text" name="cccd" id="editCccd" class="form-control" required pattern="\d{12}">
+              <label>Số CCCD *</label>
+              <input name="cccd" id="editCccd" class="form-control" required pattern="\d{12}">
             </div>
           </div>
           <div class="row mb-3">
             <div class="col-md-4">
-              <label for="editTaiKhoan">Tài khoản *</label>
-              <input type="text" name="taikhoan" id="editTaiKhoan" class="form-control" required>
+              <label>Tài khoản *</label>
+              <input name="taikhoan" id="editTaiKhoan" class="form-control" required>
             </div>
             <div class="col-md-4">
-              <label for="editMatKhau">Mật khẩu mới (để trống nếu không đổi)</label>
-              <input type="password" name="new_password" id="editMatKhau" class="form-control">
+              <label>Mật khẩu mới (để trống nếu không đổi)</label>
+              <input name="new_password" type="password" id="editMatKhau" class="form-control">
             </div>
             <div class="col-md-4">
-              <label for="editQuyen">Quyền *</label>
+              <label>Quyền *</label>
               <select name="quyen" id="editQuyen" class="form-select" required>
                 <option value="">-- Chọn quyền --</option>
                 <?php foreach ($roles as $r): ?>
