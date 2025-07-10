c535c59643f4be15acf4c6b9493ddd82c22e8d46
nguyenhuonggiang <nhgiang.0412@gmail.com>
2025-07-10 16:10:07 +0700

    Update add_taikhoan.php thêm ràng buộc

--- views/taikhoan/add_taikhoan.php
+++ views/taikhoan/add_taikhoan.php
@@ -9,27 +9,33 @@
         </div>
         <div class="modal-body">
           <div class="row mb-3">
-            <div class="col-md-4"><label>Họ tên *</label>
-              <input name="hoten"  id="editHoTen" class="form-control" required pattern="^[a-zA-ZÀ-ỹ\s]+$" title="Chỉ cho phép chữ cái và khoảng trắng">
-</div>
-            <div class="col-md-4"><label>Số điện thoại *</label><input name="sdt" class="form-control" required pattern="\d{10}"></div>
-            <div class="col-md-4"><label>Số CCCD *</label><input name="cccd" class="form-control" required pattern="\d{12}"></div>
-          </div>
-          <div class="row mb-3">
-            <div class="col-md-4"><label>Tài khoản *</label>
-               <input name="taikhoan" id="editTaiKhoan" class="form-control" required pattern="^[a-zA-Z0-9_]{4,20}$" title="Chỉ gồm chữ cái, số, dấu gạch dưới, từ 4–20 ký tự">
-            </div>
-            <div class="col-md-4"><label>Mật khẩu *</label>
-              <input name="matkhau" type="password" class="form-control" required></div>
-             <input type="password" name="matkhau" class="form-control" required
-                   pattern="(?=.*[A-Za-z])(?=.*\d).{6,}"
-                   title="Tối thiểu 6 ký tự, gồm ít nhất 1 chữ và 1 số"
-                   >
-
             <div class="col-md-4">
-              <label>Quyền *</label>
-              <select name="quyen" class="form-select" required>
-                <option value="">-- Chọn quyền --</option>
+              <label for="hoten">Họ tên *</label>
+              <input name="hoten" id="hoten" class="form-control" required pattern="^[\p{L}\s]{3,50}$" title="Họ tên phải từ 3-50 ký tự, chỉ gồm chữ và khoảng trắng">
+            </div>
+            <div class="col-md-4">
+              <label for="sdt">Số điện thoại *</label>
+              <input name="sdt" id="sdt" class="form-control" required pattern="^\d{10}$" title="Số điện thoại phải đúng 10 chữ số">
+            </div>
+            <div class="col-md-4">
+              <label for="cccd">Số CCCD *</label>
+              <input name="cccd" id="cccd" class="form-control" required pattern="^\d{12}$" title="CCCD phải đúng 12 chữ số">
+            </div>
+          </div>
+
+          <div class="row mb-3">
+            <div class="col-md-4">
+              <label for="taikhoan">Tài khoản *</label>
+              <input name="taikhoan" id="taikhoan" class="form-control" required minlength="4" maxlength="30" title="Tài khoản từ 4 đến 30 ký tự">
+            </div>
+            <div class="col-md-4">
+              <label for="matkhau">Mật khẩu *</label>
+              <input name="matkhau" id="matkhau" type="password" class="form-control" required minlength="6" maxlength="100" title="Mật khẩu từ 6 ký tự trở lên">
+            </div>
+            <div class="col-md-4">
+              <label for="quyen">Quyền *</label>
+              <select name="quyen" id="quyen" class="form-select" required>
+                <option value="" disabled selected hidden>-- Chọn quyền --</option>
                 <?php foreach ($roles as $r): ?>
                   <option value="<?= $r['ID'] ?>"><?= htmlspecialchars($r['role_name']) ?></option>
                 <?php endforeach; ?>
@@ -37,6 +43,7 @@
             </div>
           </div>
         </div>
+
         <div class="modal-footer">
           <button type="submit" class="btn btn-primary">Lưu</button>
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
